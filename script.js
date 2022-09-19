8ffb8c2d46b92accd1552671e247eb7a8868dbfb
Mabel Taiwo <seyitaiwo21@gmail.com>
2022-09-19 20:12:30 +0100

    another commit

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-// Common Js
-document.querySelectorAll('.watch-control, .controls a, .iphone-btn').forEach(control => {
-  control.addEventListener('click', e => {
-    e.preventDefault()
-  })
-})
+// Common JS
+document
+  .querySelectorAll(".watch-control, .controls a, .iphone-btn")
+  .forEach((control) => {
+    control.addEventListener("click", (e) => {
+      e.preventDefault();
+    });
+  });
+// End of Common JS
 
-// End of common Js
 // Cube
-let x = 0
-let y = 20
-let z = 0
-let bool = true
+let x = 0;
+let y = 20;
+let z = 0;
+let bool = true;
 let interval;
 
-const cube = document.querySelector('.cube')
+const cube = document.querySelector(".cube");
 
-document.querySelector('.top-x-control').addEventListener('click', () => {
-  cube.style.transform = `rotateX(${x += 20}deg) rotateY(${y}deg) rotateZ(${z}deg)`
-})
+document.querySelector(".top-x-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${(x += 20)}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+});
 
 document.querySelector(".bottom-x-control").addEventListener("click", () => {
   cube.style.transform = `rotateX(${(x -= 20)}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
 });
 
 document.querySelector(".left-y-control").addEventListener("click", () => {
-  cube.style.transform = `rotateY(${(y -= 20)}deg) rotateX(${(x)}deg) rotateZ(${z}deg)`;
+  cube.style.transform = `rotateX(${x}deg) rotateY(${(y -= 20)}deg) rotateZ(${z}deg) `;
 });
 
 document.querySelector(".right-y-control").addEventListener("click", () => {
-  cube.style.transform = `rotateY(${(y += 20)}deg) rotateX(${(x)}deg) rotateZ(${z}deg)`;
+  cube.style.transform = `rotateX(${x}deg) rotateY(${(y += 20)}deg) rotateZ(${z}deg) `;
 });
 
 document.querySelector(".top-z-control").addEventListener("click", () => {
@@ -36,180 +38,140 @@ document.querySelector(".top-z-control").addEventListener("click", () => {
 });
 
 document.querySelector(".bottom-z-control").addEventListener("click", () => {
-  cube.style.transform = `rotateY(${y}deg) rotateX(${x}deg) rotateZ(${(z += 20)}deg)`;
+  cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${(z += 20)}deg) `;
 });
 
 const playPause = () => {
-  if(bool) {
+  if (bool) {
     interval = setInterval(() => {
       cube.style.transform = `rotateX(${x}deg) rotateY(${y++}deg) rotateZ(${z}deg)`;
-    }, 100)
+    }, 100);
   } else {
-    clearInterval(interval)
+    clearInterval(interval);
   }
-  
-}
+};
 
-playPause()
+playPause();
 
-document.querySelector('.controls').addEventListener('mouseover', () => {
-  bool = false
+document.querySelector(".controls").addEventListener("mouseover", () => {
+  bool = false;
+  playPause();
+});
 
-  playPause()
-}) 
+document.querySelector(".controls").addEventListener("mouseout", () => {
+  bool = true;
+  playPause();
+});
 
-document.querySelector('.controls').addEventListener('mouseout', () => {
-  bool = true
-  playPause()
-
-})
-
-// End Of Cube
-
+// End of Cube
 
 // Slideshow
-function slideshowDivs() {
+const slideshowDivs = () => {
   for (let i = 1; i <= 5; i++) {
-    const div = document.createElement('div')
+    const div = document.createElement("div");
 
-    div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg)`
+    div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg)`;
 
-    i === 1 && div.classList.add('change')
+    i === 1 && div.classList.add("change");
 
-    document.querySelector('.slideshow').appendChild(div)
+    document.querySelector(".slideshow").appendChild(div);
   }
-}
+};
 
-slideshowDivs()
+slideshowDivs();
 
-const divs = document.querySelectorAll('.slideshow div')
+const divs = document.querySelectorAll(".slideshow div");
 
-let  a = 1
+let a = 1;
 
 const slideshow = () => {
   setInterval(() => {
-   a++
+    a++;
 
-    const div = document.querySelector('.slideshow .change')
+    const div = document.querySelector(".slideshow .change");
 
-    div.classList.remove('change')
+    div.classList.remove("change");
 
-    if(a < divs.length) {
-       div.nextElementSibling.classList.add('change')
-  } else {
-    divs[0].classList.add('change')
-    a = 1
+    if (a < divs.length) {
+      div.nextElementSibling.classList.add("change");
+    } else {
+      divs[0].classList.add("change");
+      a = 1;
+    }
+  }, 20000);
+};
+
+slideshow();
+// End of Slideshow
+
+// Section 3
+const section3Content = document.querySelector(".section-3-content");
+
+window.addEventListener("scroll", () => {
+  if (
+    window.pageYOffset + window.innerHeight >=
+    section3Content.offsetTop + section3Content.offsetHeight / 2
+  ) {
+    section3Content.classList.add("change");
   }
-    }, 2000)
-}
-
-slideshow()
-// End of slideshow
-
-// Section 3 Macbook
-const section3Content = document.querySelector('.section-3-content') 
-
-window.addEventListener('scroll', () => {
-  if(window.pageYOffset + window.innerHeight >= section3Content.offsetTop + section3Content.offsetHeight / 2) {
-    section3Content.classList.add('change')
-  }
-})
+});
 // End of Section 3
 
-// section 4
-const watchBands = document.querySelector('.watch-bands')
-const watchCases = document.querySelector('.watch-cases')
-const watchTopControl = document.querySelector('.watch-top-control')
-const watchRightControl = document.querySelector(".watch-right-control")
-const watchBottomControl = document.querySelector(".watch-bottom-control")
-const watchLeftControl = document.querySelector(".watch-left-control")
+// Section 4
+const watchBands = document.querySelector(".watch-bands");
+const watchCases = document.querySelector(".watch-cases");
 
-let axisY = 0
-let axisX = 0
+const watchTopControl = document.querySelector(".watch-top-control");
+const watchRightControl = document.querySelector(".watch-right-control");
+const watchBottomControl = document.querySelector(".watch-bottom-control");
+const watchLeftControl = document.querySelector(".watch-left-control");
+
+let axisY = 0;
+let axisX = 0;
 
 const hideControl = () => {
-  if(axisY === -280) {
-    watchTopControl.classList.add('hideControl')
+  if (axisY === -280) {
+    watchTopControl.classList.add("hideControl");
   } else {
-    watchTopControl.classList.remove("hideControl")
+    watchTopControl.classList.remove("hideControl");
   }
 
-   if (axisY === 280) {
-     watchBottomControl.classList.add("hideControl");
-   } else {
-     watchBottomControl.classList.remove("hideControl");
-   }
+  if (axisY === 280) {
+    watchBottomControl.classList.add("hideControl");
+  } else {
+    watchBottomControl.classList.remove("hideControl");
+  }
 
-    if (axisX === 280) {
-      watchRightControl.classList.add("hideControl");
-    } else {
-      watchRightControl.classList.remove("hideControl");
-    }
+  if (axisX === 280) {
+    watchRightControl.classList.add("hideControl");
+  } else {
+    watchRightControl.classList.remove("hideControl");
+  }
 
-    if (axisX === -280) {
-      watchLeftControl.classList.add("hideControl");
-    } else {
-      watchLeftControl.classList.remove("hideControl");
-    }
-}
+  if (axisX === -280) {
+    watchLeftControl.classList.add("hideControl");
+  } else {
+    watchLeftControl.classList.remove("hideControl");
+  }
+};
 
-watchTopControl.addEventListener('click', () => {
-  watchCases.style.marginTop = `${axisY -=70}rem`
-  hideControl()
-})
+watchTopControl.addEventListener("click", () => {
+  watchCases.style.marginTop = `${(axisY -= 70)}rem`;
+  hideControl();
+});
 
 watchBottomControl.addEventListener("click", () => {
-  watchCases.style.marginTop = `${(axisY += 70)}rem`
-  hideControl()
-})
+  watchCases.style.marginTop = `${(axisY += 70)}rem`;
+  hideControl();
+});
 
 watchRightControl.addEventListener("click", () => {
-  watchBands.style.marginRight = `${(axisX += 70)}rem`
-  hideControl()
-})
+  watchBands.style.marginRight = `${(axisX += 70)}rem`;
+  hideControl();
+});
 
 watchLeftControl.addEventListener("click", () => {
-  watchBands.style.marginRight = `${(axisX -= 70)}rem`
-  hideControl()
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// End of section 4
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  watchBands.style.marginRight = `${(axisX -= 70)}rem`;
+  hideControl();
+});
+// End of Section 4
